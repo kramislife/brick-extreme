7e1d9632b308911c36da995782a3ea5cabf0191e
kramislyf <egariando619@gmail.com>
2024-11-26 12:58:10 +0800

    added a scroll on top

--- frontend/src/routes/RootLayout.jsx
+++ frontend/src/routes/RootLayout.jsx
@@ -1,11 +1,15 @@
 import Header from "@/components/layout/header/Header";
 import Footer from "@/components/layout/footer/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "@/components/layout/scroll/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-brand-gradient text-light">
       <Header />
+
+      {/* Scroll to top on route change */}
+      <ScrollToTop />
       <main className="flex-grow">
 
         {/* Renders the nested route components */}
